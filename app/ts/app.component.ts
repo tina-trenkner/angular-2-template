f5d6b9e6bd244634df3f43a0db1af806ca422c0b
Tina Trenkner-Meade <tinatrenkner@gmail.com>
2016-10-16 18:14:28 +1300

    videos 1 through 7 done

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,21 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+  mainHeading = Config.MAIN_HEADING;
+  videos:Array<Video>;
+
+  constructor(){
+    this.videos = [
+      new Video(1, "Angular part 7", "f8qBeaGe2S4", "how to nest components in Angular")
+    ]
+  }
+}
